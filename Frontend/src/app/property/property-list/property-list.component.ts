0ee1a670cba86ada96f776cf4869ebe0a6485927
Rajiv Chockalingam <rajivchockalingam@Rajivs-MBP.hsd1.tx.comcast.net>
2020-08-01 20:31:52 -0500

    Added IProperty interface

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -7,7 +8,7 @@ import { HousingService } from 'src/app/services/housing.service';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  Properties: any;
+  Properties: Array<IProperty>;
   constructor(private housingService: HousingService) {}
 
   ngOnInit(): void {
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { IProperty } from '../property/IProperty.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HousingService {
   constructor(private http: HttpClient) {}
-  getAllProperties() {
-    return this.http.get('data/properties.json');
+
+  getAllProperties(): Observable<IProperty[]> {
+    return this.http.get('data/properties.json').pipe(
+      map((data) => {
+        let propertiesArray: Array<IProperty> = [];
+        for (let id in data) {
+          if (data.hasOwnProperty(id)) {
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    );
   }
 }
